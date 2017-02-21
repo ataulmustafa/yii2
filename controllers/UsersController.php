2a27279aad99f9ab8711ed7813321cd0849717fe
Ata ul Mustafa <ata_ul_mustafa@hotmail.com>
2017-02-21 20:59:04 +0500

    Comment added.

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -13,6 +13,7 @@ use app\models\Users;
 
 class UsersController extends Controller{
     public function actionIndex(){
+        // Show users
         $users = Users::find()->all();
 
         return $this->render('index', ['users'=>$users]);
